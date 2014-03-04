46823f7a5925c0ee44b8ad63e9945b5acfab2443
mistday <mistday@yandex.ru>
2014-03-04 04:00:01 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ var querystring = require('querystring');
 var server = http.createServer();
 
 server.on('request', function(req, res){
+	var body = '';
 	console.log(req.method);
-	req.on('readable', function() {
-			var body = querystring.parse(req.read());
+	req
+		.on('readable', function() {
+			body += req.read();
+		})
+		.on('end', function() {
+			body = querystring.parse(body);
 			console.dir(body);
-	});
+		});
 	var content = '<form action="" method="post">'+
 					'<input type="text" name="textfild">'+
 					'<input type="password" name="passfild">'+

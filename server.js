0dda7962fdd6cf786806e991dd43409446ecf216
mistday <mistday@yandex.ru>
2014-03-04 03:52:09 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var querystring = require('querystring');
+
+var server = http.createServer();
+
+server.on('request', function(req, res){
+	console.log(req.method);
+	req.on('readable', function() {
+			var body = querystring.parse(req.read());
+			console.dir(body);
+	});
+	var content = '<form action="" method="post">'+
+					'<input type="text" name="textfild">'+
+					'<input type="password" name="passfild">'+
+					'<input type="submit" value="post"></form>';
+	res.setHeader('Content-Type', 'text/html');
+	res.end(content);
+});
+server.listen(8000);
